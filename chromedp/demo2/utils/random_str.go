287993d71e14fbb540e91b9a75381f6c26925eb2
mengjiaheng <mengjiaheng@asants.com>
2021-07-14 14:35:35 +0800

    新增随机数函数

---
+++ chromedp/demo2/utils/random_str.go
@@ -0,0 +1,14 @@
+package utils
+
+import "math/rand"
+
+const letterBytes = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+//产生随机数
+func RandomString() string {
+	b := make([]byte, rand.Intn(10)+10)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
